aab94c6a8843677a4953c0a2e195aa2e0e8dbc8c
Rishav Goyal <rishavgoyal0407@gmail.com>
2026-01-18 11:47:33 +0530

    add user model and connect db

---
+++ server/models/User.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+ const userSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    role:{
+        type:String,
+        enum:["admin","employee"],
+        default:"employee"
+    },
+    employeeId:{
+        type:String,
+        unique:true
+    },
+    department:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Department"
+    },
+    isActive:{
+        type:Boolean,
+        default:true
+    },
+
+ },{timestamps:true})
+
+ const User = mongoose.model("user",userSchema)
+
+ export default User;
